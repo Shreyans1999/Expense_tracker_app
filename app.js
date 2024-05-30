4ee635794ff70f7b47db16888cc4d5d976130267
shreyans <ssaklecha@primesoft.net>
2024-05-30 13:27:03 +0530

    Make the backend api of the login with some final changes

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const sq = require('./backend/util/database')
-sq.sync()
-const Router = require('./backend/routes/router')
+const sq = require('./backend/util/database');
+const Router = require('./backend/routes/router');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 app.use(bodyParser.json({ extended: false }));
 app.use(cors());
-app.use(Router)
+app.use(Router);
 
+// Serve static files from the "frontend" directory
+app.use('/frontend', express.static(path.join(__dirname, 'frontend')));
 
-app.listen(3000);
+sq.sync()
+  .then(response => {
+    console.log('Database synced:', response);
+  })
+  .catch(err => {
+    console.log('Database sync error:', err);
+  });
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
