f7a72f73cd486cb9a035b7952f5f37fc992cc5f9
shreyans <ssaklecha@primesoft.net>
2024-08-15 01:20:46 +0530

    added some modifications in passcontroller

--- backend/controller/passController.js
+++ backend/controller/passController.js
@@ -15,89 +15,113 @@ exports.forgetPassword = async (req, res) => {
     });
   }
   console.log(mail);
-  const client = Sib.ApiClient.instance;
-  const apiKey = client.authentications["api-key"];
+  var defaultClient = Sib.ApiClient.instance
+  
+  var apiKey = defaultClient.authentications['api-key'];
+
   apiKey.apiKey = process.env.SIB_API_KEY;
 
   const TranEmailApi = new Sib.TransactionalEmailsApi();
-  const sender = { email: "solutions9584@gmail.com" };
-  const recievers = [{ email: mail }];
+
+  const sender = {
+    email: "solutions9584@gmail.com",
+  };
+  const recievers = [
+    {
+      email: mail,
+    },
+  ];
   const uid = uuidv4();
   const UId = user.dataValues.id
   console.log(`id is -------${UId}`);
-  await ForgetPassword.create({ id: uid, UserId: UId, active: true });
-  
-  TranEmailApi.sendTransacEmail({
-    sender,
-    to: recievers,
-    subject: "Password Reset Mail",
-    textContent: `https://localhost:3000/reset-password/${uid}`,
-  })
-    .then(response => {
-      console.log(response);
-      res.status(201).json(response);
+  ForgetPassword.create({
+    id: uid,
+   UserId: UId,
+    active: true,
+  }).then(() => {
+    TranEmailApi.sendTransacEmail({
+      sender,
+      to: recievers,
+      subject: "Password Reset Mail",
+      textContent: `http://localhost:3000/reset-password/${uid}`,
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  res.status(201).json(response);
 };
 
 exports.resetpassword = (req, res) => {
   const Uid = req.params.uid;
   console.log(`uidis${Uid}`);
-  ForgetPassword.findOne({ where: { id: Uid } }).then(forgotpasswordrequest => {
-    if (forgotpasswordrequest) {
-      forgotpasswordrequest.update({ active: false });
-      res.status(200).send(`<html>
-                              <script>
-                                  function formsubmitted(e){
-                                      e.preventDefault();
-                                      console.log('called');
-                                  }
-                              </script>
-                              <form action="/updatepassword/${Uid}" method="post">
-                                  <label for="newpassword">Enter New password</label>
-                                  <input name="newpassword" type="password" required></input>
-                                  <button>reset password</button>
-                              </form>
-                          </html>`);
-      res.end();
+  ForgetPassword.findOne({ where: { id: Uid } }).then(
+    (forgotpasswordrequest) => {
+      if (forgotpasswordrequest) {
+        forgotpasswordrequest.update({ active: false });
+        res.status(200).send(`<html>
+                                    <script>
+                                        function formsubmitted(e){
+                                            e.preventDefault();
+                                            console.log('called')
+                                        }
+                                    </script>
+                                    <form action="/updatepassword/${Uid}" method="get">
+                                        <label for="newpassword">Enter New password</label>
+                                        <input name="newpassword" type="password" required></input>
+                                        <button>reset password</button>
+                                    </form>
+                                </html>`);
+        res.end();
+      }
     }
-  });
+  );
 };
 
 exports.updatepassword = (req, res) => {
   try {
     const { newpassword } = req.query;
     console.log(`newpass-${newpassword}`);
+
     const { resetpasswordid } = req.params;
     console.log(`reset id-${resetpasswordid}`);
-    ForgetPassword.findOne({ where: { id: resetpasswordid } }).then(resetpasswordrequest => {
-      console.log(resetpasswordrequest);
-      User.findOne({ where: { id: resetpasswordrequest.UserId } }).then(user => {
-        if (user) {
-          const saltRounds = 10;
-          bcrypt.genSalt(saltRounds, (err, salt) => {
-            if (err) {
-              console.log(err);
-              throw new Error(err);
-            }
-            bcrypt.hash(newpassword, salt, (err, hash) => {
-              if (err) {
-                console.log(err);
-                throw new Error(err);
-              }
-              user.update({ password: hash }).then(() => {
-                res.status(201).json({ message: "Successfully updated the new password" });
+    ForgetPassword.findOne({ where: { id: resetpasswordid } }).then(
+      (resetpasswordrequest) => {
+        console.log(resetpasswordrequest);
+        User.findOne({ where: { id: resetpasswordrequest.UserId } }).then(
+          (user) => {
+            if (user) {
+              const saltRounds = 10;
+              bcrypt.genSalt(saltRounds, function (err, salt) {
+                if (err) {
+                  console.log(err);
+                  throw new Error(err);
+                }
+                bcrypt.hash(newpassword, salt, function (err, hash) {
+                  if (err) {
+                    console.log(err);
+                    throw new Error(err);
+                  }
+                  user.update({ password: hash }).then(() => {
+                    res
+                      .status(201)
+                      .json({ message: "Successfuly update the new password" });
+                  });
+                });
               });
-            });
-          });
-        } else {
-          return res.status(404).json({ error: "No user exists", success: false });
-        }
-      });
-    });
+            } else {
+              return res
+                .status(404)
+                .json({ error: "No user Exists", success: false });
+            }
+          }
+        );
+      }
+    );
   } catch (error) {
     return res.status(403).json({ error, success: false });
   }
